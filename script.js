0262c92e6a9bb692f894dbf765f07cb8c4427d69
Abenezer Haile <devabenezer7@gmail.com>
2025-02-11 19:18:24 +0300

    finished the input boxes

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const inputFieldContainer = document.getElementById("input-field-container");
 const inputFieldTemplate = document.getElementById("input-field-template");
-const textBox = document.getElementById("text-box");
-const randomWordBtn = document.getElementById("randomBtn");
+const randomWordElement = document.getElementById("randomWord");
 const checkWordBtn = document.getElementById("checkBtn");
 const restartBtn = document.getElementById("restart");
 const message = document.getElementById("message");
-const mistakesElement = document.getElementById("mistakes");
+const highScoreElement = document.getElementById("highScore");
 const triesboxesElement = document.querySelectorAll("#tries-box > div ");
 const triesElement = document.getElementById("tries");
 
@@ -22,12 +21,10 @@ fetch("./words.json")
   });
 
 let currentWord = "";
-let tries = 0;
-let mistakes = "";
-let currentInputIndex = 0;
-let userInput = "";
-let wrongWords = [];
-
+let tries = 5;
+let highScore = 0;
+let mistakesIndexes = [];
+triesElement.innerHTML = `(${tries}/5)`;
 function scrambleWord(word) {
   if (typeof word != "string" || word.length == 0) {
     return "";
@@ -42,14 +39,13 @@ function scrambleWord(word) {
 }
 
 function generateRandomWord() {
+  setTimeout(() => (message.innerHTML = ""), 2000);
   // Generate and display scrambled word
-  clearAll();
-
   const randomWordIndex = Math.floor(Math.random() * words.length);
   currentWord = words[randomWordIndex];
   const scrambledWord = scrambleWord(currentWord);
   createInputFields(scrambledWord.length);
-  textBox.textContent = scrambledWord;
+  randomWordElement.textContent = scrambledWord;
 }
 
 function createInputFields(length) {
@@ -59,153 +55,121 @@ function createInputFields(length) {
     const inputFieldTemplateClone = inputFieldTemplate.content.cloneNode(true);
     inputFieldContainer.appendChild(inputFieldTemplateClone);
   }
-  inputFieldContainer.children[0].setAttribute("id", "active-input");
-  inputFieldContainer.children[0].placeholder = "_";
+  const inputFieldElements = inputFieldContainer.querySelectorAll("input");
+  inputFieldContainer[0].focus();
+  inputFieldElements[0].setAttribute("id", "active-input");
+  addBoxesBahvior(inputFieldElements);
 }
 
-function handleInput(event) {
-  console.log("even");
-  message.innerHTML = "";
-
-  const inputBoxes = inputFieldContainer.querySelectorAll("input");
-  //handling user Input
-  inputBoxes.forEach((box, index) => {
+function addBoxesBahvior(boxes) {
+  boxes.forEach((box, index) => {
+    box.addEventListener("click", () => {
+      boxes.forEach((box, index) => {
+        box.removeAttribute("id");
+      });
+      box.setAttribute("id", "active-input");
+      box.focus();
+    });
     box.addEventListener("input", (e) => {
-      if (e.target.value && index < inputBoxes.length - 1) {
-        inputBoxes[index + 1].focus();
-        inputBoxes[index].removeAttribute("id");
-        inputBoxes[index + 1].setAttribute("id", "active-input");
-        inputBoxes[index + 1].placeholder = "_";
+      if (e.target.value.length === 1 && index < boxes.length - 1) {
+        box.removeAttribute("id");
+        boxes[index + 1].setAttribute("id", "active-input");
+        boxes[index + 1].focus();
       }
     });
-  });
 
-  inputBoxes.forEach((box, index) => {
     box.addEventListener("keydown", (e) => {
-      if (e.key === "Backspace" && !box.value && index > 0) {
+      if (e.key === "Backspace" && index > 0) {
+        boxes[index - 1].focus();
+        boxes[index - 1].setAttribute("id", "active-input");
+        box.removeAttribute("id");
         e.target.value = "";
-        inputBoxes[index - 1].focus();
-        inputBoxes[index - 1].setAttribute("id", "active-input");
-        inputBoxes[index].removeAttribute("id");
-        inputBoxes[index].placeholder = "";
-        if (index < inputBoxes.length - 1) {
-          inputBoxes[index + 1].removeAttribute("id");
-          inputBoxes[index + 1].placeholder = "";
-        }
-      } else if (e.key === "ArrowLeft" && index > 0) {
-        inputBoxes[index - 1].focus();
-        inputBoxes[index - 1].setAttribute("id", "active-input");
-        inputBoxes[index].removeAttribute("id");
+      } else if (e.key === "Backspace" && index === 0) {
+        e.target.value = "";
+      }
 
-        if (!inputBoxes[index - 1].value) {
-          inputBoxes[index - 1].placeholder = "_";
-        }
-        if (!inputBoxes[index].value) {
-          inputBoxes[index].placeholder = "";
-        }
-        if (index < inputBoxes.length - 1) {
-          inputBoxes[index + 1]?.removeAttribute("id");
-        }
-      } else if (e.key == "ArrowRight" && index < inputBoxes.length - 1) {
-        inputBoxes[index + 1].focus();
-        inputBoxes[index + 1].setAttribute("id", "active-input");
-        inputBoxes[index].removeAttribute("id");
-        console.log(inputBoxes[index].value);
-        if (!inputBoxes[index + 1].value) {
-          inputBoxes[index + 1].placeholder = "_";
-        }
-        if (!inputBoxes[index].value) {
-          inputBoxes[index].placeholder = "";
-        }
-        if (index < inputBoxes.length - 1) {
-          inputBoxes[index - 1]?.removeAttribute("id");
-        }
+      if (e.key === "ArrowLeft" && index > 0) {
+        box.removeAttribute("id");
+        boxes[index - 1].focus();
+        boxes[index - 1].setAttribute("id", "active-input");
+      }
+
+      if (e.key === "ArrowRight" && index < boxes.length - 1) {
+        box.removeAttribute("id");
+        boxes[index + 1].setAttribute("id", "active-input");
+        boxes[index + 1].focus();
       }
     });
   });
-
-  if (event.key === "Enter") {
-    checkAnswer(inputBoxes);
-  }
 }
+function handleInput(event) {}
 
 function checkAnswer(boxes) {
-  mistakes = [];
-  mistakesElement.innerHTML = "";
-  if (tries == 5) {
-    message.innerHTML = "You lost you are such a loser, cray baby";
-    message.style.color = "#6b7280";
-    return;
-  }
-  userInput = "";
+  let userInput = "";
+  let score = 0;
+
+  //check if the boxes are all filled
   for (let i = 0; i < boxes.length; i++) {
-    if (!boxes[i].value || boxes[i].value == "") {
-      message.innerHTML = "please fill out all the boxes";
-      message.style.color = "#eab308";
+    if (boxes[i].value === null || boxes[i].value === "") {
+      tries--;
+      message.innerHTML = "please fill out all of the words";
+      message.style.color = "yellow";
       return;
     }
-    userInput += boxes[i].value;
   }
-  if (userInput.toLowerCase() !== currentWord.toLowerCase()) {
-    message.innerHTML = "wrong answer!! is that all you got I'm sorry for you ";
-    message.style.color = "#ef4444";
-    const wrongLetter = [];
-    for (let i = 0; i < userInput.length; i++) {
-      if (userInput[i] != currentWord[i]) {
-        wrongLetter.push(userInput[i]);
+
+  tries--;
+  //Build user input
+  boxes.forEach((box) => (userInput += box.value.toLowerCase()));
+
+  triesboxesElement[tries].style.backgroundColor = "#4A5567";
+  triesElement.innerHTML = `(${tries}/5)`;
+  //check answer
+  if (userInput !== currentWord) {
+    if (tries === 0) {
+      return restart();
+    }
+    message.innerHTML = "wrong answer try again";
+    message.style.color = "red";
+    for (let i = 0; i < currentWord.length; i++) {
+      if (currentWord[i] !== userInput[i]) {
+        boxes[i].classList.add("wrongAnswer");
       }
     }
-    mistakes = "";
-    mistakesElement.innerHTML = "";
-    mistakes = wrongLetter.join(",");
-    wrongLetter.splice(0, wrongLetter.length);
-
-    mistakesElement.innerHTML = ` ${mistakes}`;
-    tries += 1;
-    triesElement.innerHTML = `(${tries}/5)`;
-    fillTryBoxes();
   } else {
-    message.innerHTML =
-      "You won! you almost made it by chance you won't be making alive next time";
-    message.style.color = "#7429C6";
+    score++;
+    highScore = highScore >= score ? highScore : score;
+    highScoreElement.innerHTML = highScore;
+    triesElement.innerHTML = "(5/5)";
+    message.innerHTML = "You won";
+    message.style.color = "green";
+    generateRandomWord();
   }
 }
-function fillTryBoxes() {
-  for (let i = 0; i < tries; i++) {
-    console.log(triesboxesElement[i]);
-    triesboxesElement[i].style.backgroundColor = "#7429C6";
-  }
-}
-function clearTryBoxes() {
-  triesboxesElement.forEach((box) => {
-    box.style.backgroundColor = "#4A5567";
-  });
-}
+
 function restart() {
   clearAll();
   generateRandomWord();
 }
 function clearAll() {
+  tries.innerHTML = "(5/5)";
   message.innerHTML = "";
-  mistakesElement.innerHTML = "";
-  currentInputIndex = 0;
-  tries = 0;
-  tries.innerHTML = "(0/5)";
+  tries = 5;
   clearTryBoxes();
-  userInput = "";
-  mistakes = [];
-  userInput = [];
+  mistakesIndexes = [];
+  triesboxesElement.forEach((box) => (box.style.backgroundColor = "#7429C6"));
+}
+function clearTryBoxes() {
+  triesboxesElement.forEach((box) => {
+    box.classList.remove("bg-steelGray");
+    box.classList.add("bg-deepViolet");
+  });
 }
 window.addEventListener("keypress", handleInput);
-randomWordBtn.addEventListener("click", () => {
-  generateRandomWord();
-  message.innerHTML = "yeah, maybe running away is you best option! pathetic ";
-  message.style.color = "#F2F5F9";
-});
-checkWordBtn.addEventListener("click", () =>
-  checkAnswer(inputFieldContainer.querySelectorAll("input"))
-);
-/* restartBtn.addEventListener("click", restart); */
 
-// Initial load
+checkWordBtn.addEventListener("click", () => {
+  checkAnswer(inputFieldContainer.querySelectorAll("input"));
+});
+restartBtn.addEventListener("click", restart);
+
 generateRandomWord();
